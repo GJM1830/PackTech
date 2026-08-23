b24627b734abfa2f0b74c20e7103491d07ab9738
Gerardo <a20222351@pucp.edu.pe>
2026-08-23 18:58:31 -0500

    Visual

--- frontend/src/GenerarPDFCotizacion.js
+++ frontend/src/GenerarPDFCotizacion.js
@@ -42,7 +42,7 @@ export async function generarPDFCotizacion(orden) {
   doc.line(M, y + 16, M + ANCHO, y + 16)
 
   if (logoBase64) {
-    doc.addImage(logoBase64, 'PNG', M, y - 2, 66, 18)
+    doc.addImage(logoBase64, 'PNG', M, y - 2, 66, 16)
   } else {
     doc.setFont('helvetica', 'bold')
     doc.setFontSize(16)
